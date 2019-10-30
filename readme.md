da28c3f0c58973ac56e1d93802bfc172b013d70f
justoxh <justoxh@gmail.com>
2019-10-30 10:42:39 +0800

    rm nodejs server

--- readme.md
+++ readme.md
@@ -1,7 +1,1 @@
 # VoteCocosBCX
-
-### mongo
-- docker run -p 28017:27017 -v $PWD/data/db:/data/db -d mongo
-
-### config
-- config/default.js
--- routes/committee.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('none');
-});
-
-module.exports = router;
