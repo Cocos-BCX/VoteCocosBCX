7a8fab67b8efb9d054a1bca8e1d42f3960af1f7e
mechaniclin <mechaniclin@163.com>
2020-02-26 18:06:04 +0800

    modify bug

--- web/vote-pro-pc/build/webpack.dev.conf.js
+++ web/vote-pro-pc/build/webpack.dev.conf.js
@@ -31,8 +31,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     hot: true,
     contentBase: false, // since we use CopyWebpackPlugin.
     compress: true,
-    // host: HOST || config.dev.host,
-    host: '192.168.15.39',
+    host: HOST || config.dev.host,
     port: PORT || config.dev.port,
     open: config.dev.autoOpenBrowser,
     overlay: config.dev.errorOverlay
