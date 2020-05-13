5ffe9a49d79928b1efcdc5c50aaea3e0cd37bc24
justoxh <justoxh@gmail.com>
2020-05-13 16:49:51 +0800

    update witness

--- srv/models/block.go
+++ srv/models/block.go
@@ -3,15 +3,19 @@ package models
 import (
 	"context"
 	"go.mongodb.org/mongo-driver/bson"
+	"log"
 	"time"
 )
 
 type WitnessGeneratedBlockNumber struct {
 	Id  string `bson:"_id,omitempty"`
-	WitnessAccountName string `json:"witness_account_name"`
-	WitnessAccountId string `json:"witness_account_id"`
-	WitnessId string `json:"witness_id"`
-	Total string  `json:"total"`
+	LastBlockTime string `json:"last_block_time"`
+	WitnessAccountName string `json:"witness_account_name" bson:"witness_account_name"`
+	Total string  `json:"total" bson:"total"`
+	LastBlock string `json:"last_block"`
+	WitnessAccountId string `json:"witness_account_id" bson:"witness_account_id"`
+	WitnessId string `json:"witness_id" bson:"witness_id"`
+
 }
 
 func FindWitnessesGeneratedBlockNumber() (map[string]WitnessGeneratedBlockNumber, error) {
@@ -20,14 +24,20 @@ func FindWitnessesGeneratedBlockNumber() (map[string]WitnessGeneratedBlockNumber
 	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
 	cursor, err := BlockCollection.Find(ctx, query)
 	if err != nil {
+		log.Println(err)
 		return results, err
 	}
-	var res []WitnessGeneratedBlockNumber
-	if err = cursor.All(ctx, &res); err != nil {
-		return results, err
-	}
-	for _, result := range res {
-		results[result.WitnessId] = result
+	defer func() {
+		_ = cursor.Close(ctx)
+	}()
+	for cursor.Next(ctx) {
+		var data WitnessGeneratedBlockNumber
+		err := cursor.Decode(&data)
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		results[data.WitnessId] = data
 	}
 	return results, nil
 }
